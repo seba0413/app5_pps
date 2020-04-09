936f4ee3a08e64486334b7ca41dee94e7e8667ee
seba0413 <sebastianlpasquini@gmail.com>
2020-04-09 12:52:31 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule.initializeApp( environment.firebaseConfig ),
+    AngularFirestoreModule,
+    AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: 'AIzaSyCs7Up9kk9sU3Ad13Ve35GtSP5dmPVl9K8',
+    authDomain: 'app5pps.firebaseapp.com',
+    databaseURL: 'https://app5pps.firebaseio.com',
+    projectId: 'app5pps',
+    storageBucket: 'app5pps.appspot.com',
+    messagingSenderId: '460489882975',
+    appId: '1:460489882975:web:6d1dd837a0487912220aea'
+  }
 };
 
 /*
